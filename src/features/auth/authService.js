5b9546c6cbaed1cb0b7cedaff46b8e405594941c
Elizabeth-Cz <87705292+Elizabeth-Cz@users.noreply.github.com>
2023-02-15 16:58:36 +0100

    Update authService.js

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://bingofy-be.onrender.com/api/users/';
+const API_URL = 'https://bingofy-be.onrender.com/api/users/';
 
 // Register user
 const register = async (userData) => {
